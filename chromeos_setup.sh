fd37ef0b6569eec458206860dd0eda9ddf11ef1d
Luis Alves <377472+lafa@users.noreply.github.com>
2018-11-16 09:47:59 -0800

    Update chromeos_setup.sh

--- chromeos_setup.sh
+++ chromeos_setup.sh
@@ -1,13 +1,14 @@
 apt update
 apt upgrade
-echo "deb https://storage.googleapis.com/cros-packages stretch main" &gt; /etc/apt/sources.list.d/cros.list
+echo "deb https://storage.googleapis.com/cros-packages stretch main" > /etc/apt/sources.list.d/cros.list
 
 if [ -f /dev/.cros_milestone ]; then sudo sed -i "s?packages?packages/$(cat /dev/.cros_milestone)?" /etc/apt/sources.list.d/cros.list; fi
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1397BC53640DB551
 apt update
 apt install binutils
-apt download cros-ui-config # ignore any warning messages
+echo "### ignore any warning messages ###"
+apt download cros-ui-config 
 ar x cros-ui-config_0.12_all.deb data.tar.gz
 gunzip data.tar.gz
 tar f data.tar --delete ./etc/gtk-3.0/settings.ini
