5289a8272639152a6f2141b8a098a5ad83638c66
Luis Alves <377472+lafa@users.noreply.github.com>
2018-11-18 02:11:28 -0800

    Update container_setup.sh

--- container_setup.sh
+++ container_setup.sh
@@ -1,14 +1,19 @@
+CONTAINER_NAME=cosmic
+
 lxc image copy ubuntu:18.10 local: --alias ubuntu1810
-lxc stop --force cosmic
-lxc delete cosmic
-lxc launch ubuntu1810 cosmic
+lxc stop --force $CONTAINER_NAME
+lxc delete $CONTAINER_NAME
+lxc launch ubuntu1810 $CONTAINER_NAME
 echo "Ignore warning messages"
 sleep 5
-lxc exec cosmic -- rm /tmp/chromeos_setup.sh
-lxc exec cosmic -- curl -o /tmp/chromeos_setup.sh https://raw.githubusercontent.com/lafaspot/chromeOSUbuntu/master/chromeos_setup.sh
-lxc exec cosmic -- bash /tmp/chromeos_setup.sh
+lxc exec $CONTAINER_NAME -- rm /tmp/chromeos_setup.sh
+lxc exec $CONTAINER_NAME -- curl -o /tmp/chromeos_setup.sh https://raw.githubusercontent.com/lafaspot/chromeOSUbuntu/master/chromeos_setup.sh
+lxc exec $CONTAINER_NAME -- bash /tmp/chromeos_setup.sh
 lxc list
 
+echo "Use the following command to login to you container"
+echo "lxc exec $CONTAINER_NAME -- su --login USER"
+
 # to make the ubuntu the default container
-# lxc stop penguin; lxc rename penguin origPenguin; lxc rename cosmic penguin
+# lxc stop penguin; lxc rename penguin origPenguin; lxc rename $CONTAINER_NAME penguin
 
