4812cccf96a342c7d5c6269eea49b71972a0e64a
Luis Alves <377472+lafa@users.noreply.github.com>
2018-11-18 00:51:56 -0800

    Update container_setup.sh

--- container_setup.sh
+++ container_setup.sh
@@ -2,6 +2,6 @@ lxc image copy ubuntu:18.10 local: --alias ubuntu1810
 lxc stop --force cosmic
 lxc delete cosmic
 lxc launch ubuntu1810 cosmic
-lxc exec cosmic -- rm /tmp/container_setup.sh
-lxc exec cosmic -- curl -o /tmp/container_setup.sh https://raw.githubusercontent.com/lafaspot/chromeOSUbuntu/master/container_setup.sh
-lxc exec cosmic -- bash /tmp/container_setup.sh
+lxc exec cosmic -- rm /tmp/chromeos_setup.sh
+lxc exec cosmic -- curl -o /tmp/chromeos_setup.sh https://raw.githubusercontent.com/lafaspot/chromeOSUbuntu/master/chromeos_setup.sh
+lxc exec cosmic -- bash /tmp/chromeos_setup.sh
