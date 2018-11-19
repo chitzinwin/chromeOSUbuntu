07001bab43299b90aa28ca19838b475d5e9053b6
Luis Alves <377472+lafa@users.noreply.github.com>
2018-11-18 21:09:25 -0800

    Update chromeos_setup.sh

--- chromeos_setup.sh
+++ chromeos_setup.sh
@@ -63,6 +63,5 @@ loginctl enable-linger $USER
 sed -i "s/ubuntu/$USER/" /etc/sudoers.d/90-cloud-init-users
 
 loginctl enable-linger $USER
-echo "Shutswon the container in progress, type 'lxd exec -- bash' to login into the container"
 sleep 3
-shutdown -h now
+
